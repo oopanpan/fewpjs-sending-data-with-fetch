0f2f789c761087308cfae55b058a19cf50dc1052
oopanpan <oopanpan@gmail.com>
2021-02-25 13:47:41 -0800

    commit tree db973c0ba6a986b71109226956e5812ced12ccef

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your code here
+const data = {name: "Steve", email: "steve@steve.com"};
+const configObj = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(data)
+};
+
+function submitData(){
+    fetch("http://localhost:3000/users", configObj)
+    .then(r => r.json())
+    .then( function (object)  {
+        document.body.innerHTML = object[ "id" ]
+      } )
+    ;
+}
+
+submitData()
